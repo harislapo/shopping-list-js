2b04be7500c99e7e0cf3e8b5c9b4ad8ca2aca7e6
harislapo <haris.lapo@gmail.com>
2023-08-16 17:21:27 +0200

    Started with adding JS.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get items
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.shopping-form');
+const shoppingItem = document.getElementById('shopping-input-item');
+const container = document.querySelector('.shopping-list-container');
+const list = document.querySelector('.shopping-list');
+const addBtn = document.querySelector('.add-btn');
+const resetBtn = document.querySelector('.reset-btn');
