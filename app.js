80295f668fd338467d90a2ba180fac92c88c76f8
harislapo <haris.lapo@gmail.com>
2023-08-16 17:43:28 +0200

    Added alerts styling, started with adding items logic.

--- app.js
+++ app.js
@@ -6,3 +6,30 @@ const container = document.querySelector('.shopping-list-container');
 const list = document.querySelector('.shopping-list');
 const addBtn = document.querySelector('.add-btn');
 const resetBtn = document.querySelector('.reset-btn');
+
+// Editing variables
+let editingItem;
+let isEditing = false;
+let editID = '';
+
+const getRandomID = () => {
+  return new Date().getTime().toString();
+};
+
+const addItem = (e) => {
+  e.preventDefault();
+  const value = shoppingItem.value;
+  const id = getRandomID();
+
+  if (value && !isEditing) {
+    console.log('Add item to the list.');
+  } else if (value && isEditing) {
+    console.log('Editing.');
+  } else {
+    alert.textContent = 'Empty value!';
+    alert.classList.add('alert-danger');
+  }
+};
+
+// Submit form
+form.addEventListener('submit', addItem);
